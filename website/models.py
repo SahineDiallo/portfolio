474feafc9e87ac38bf6144b20516b464583dc7d6
Sahine Diallo <psdiallo300@gmail.com>
2022-04-29 12:50:53 +0000

    added the database models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,16 @@
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+class Files(models.Model):
+    image = models.ImageField(upload_to='project_images')
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    overview = models.TextField()
+    thumbnail = models.ForeignKey(Files, on_delete=models.CASCADE, related_name='proj')
+    gallery = models.ManyToManyField(Files, related_name='projs')
+    description = models.TextField()
+    github = models.URLField()
+    website = models.URLField(blank=True, null=True)
+
